5eaa7c496d3d1388e9967e82c3bc115b20f5444b
tinzulu <tinzulu@gmail.com>
2022-02-07 23:58:44 +0200

    initial commit

---
+++ src/main/java/com/msu/voters_roll/VotersRollApplication.java
@@ -0,0 +1,13 @@
+package com.msu.voters_roll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotersRollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotersRollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msu/voters_roll/VotersRollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msu.voters_roll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotersRollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
