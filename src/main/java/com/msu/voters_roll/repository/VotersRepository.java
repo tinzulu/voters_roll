dcabc2ccedf780288150b08dee3c8ac73cb95ee2
tinzulu <tinzulu@gmail.com>
2022-02-08 19:59:58 +0200

    added exitsByNationalid function in repo

--- src/main/java/com/msu/voters_roll/repository/VotersRepository.java
+++ src/main/java/com/msu/voters_roll/repository/VotersRepository.java
@@ -1,9 +1,13 @@
 package com.msu.voters_roll.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.msu.voters_roll.model.Voters;
 
 public interface VotersRepository extends JpaRepository<Voters, Integer>{
-
+	
+	@Query("SELECT COUNT(v) FROM voters v WHERE i.nationalid =:nationalid")
+	public int exitsByNationalid(@Param("nationalid")  String nationalId);
 }
