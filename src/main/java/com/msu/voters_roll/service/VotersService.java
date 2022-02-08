a554f0413c96191c7cd4a7eb5b1d27ed2f194f30
tinzulu <tinzulu@gmail.com>
2022-02-08 21:10:18 +0200

    completed Voter services task

---
+++ src/main/java/com/msu/voters_roll/service/VotersService.java
@@ -0,0 +1,43 @@
+package com.msu.voters_roll.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.msu.voters_roll.dto.VoterRequest;
+import com.msu.voters_roll.model.Voter;
+import com.msu.voters_roll.repository.VotersRepository;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class VotersService {
+	private final VotersRepository votersRepo;
+	
+	public Voter save(Voter voter) {
+		int num = votersRepo.exitsByNationalid(voter.getNationalId());
+		
+		if(num >= 1) {
+			log.warn("Duplicate national id "+voter.getNationalId());
+		} else {
+			Voter newVoter = votersRepo.save(voter);
+			return newVoter;
+		}
+		return voter;
+	}
+	
+	public List<Voter> getVoters(){
+		return votersRepo.findAll();
+	}
+	
+	public Voter getVoterBySurnameAndId(VoterRequest request) {
+		return votersRepo.findByNationalIdAndSurname(request.getNationalId(), request.getSurname());
+	}
+	
+	
+	
+	
+}
