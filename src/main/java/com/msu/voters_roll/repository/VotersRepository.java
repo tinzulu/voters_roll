8ef6451f50f7a4c8f2aabd5fa0febb3e003f3c13
tinzulu <tinzulu@gmail.com>
2022-02-08 20:16:37 +0200

    added searchbynationalid

--- src/main/java/com/msu/voters_roll/repository/VotersRepository.java
+++ src/main/java/com/msu/voters_roll/repository/VotersRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.msu.voters_roll.model.Voters;
+import com.msu.voters_roll.model.Voter;
 
-public interface VotersRepository extends JpaRepository<Voters, Integer>{
+public interface VotersRepository extends JpaRepository<Voter, Integer>{
 	
 	@Query("SELECT COUNT(v) FROM voters v WHERE i.nationalid =:nationalid")
 	public int exitsByNationalid(@Param("nationalid")  String nationalId);
