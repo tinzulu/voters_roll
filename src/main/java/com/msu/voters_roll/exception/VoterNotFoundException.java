81cc7ad0e73ecaae8f7fd6510c11092dada63e82
tinzulu <tinzulu@gmail.com>
2022-02-09 12:20:36 +0200

    created voter exception

---
+++ src/main/java/com/msu/voters_roll/exception/VoterNotFoundException.java
@@ -0,0 +1,8 @@
+package com.msu.voters_roll.exception;
+
+public class VoterNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	public VoterNotFoundException(String message) {
+		
+	}
+}
